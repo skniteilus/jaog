5bf820d7dda8c4ff0f63673aa07172c2a8aa343d
Stephan Knitelius <stephan@knitelius.com>
2012-02-18 21:23:20 +0100

    Added targetCSV to Annotation

--- jaog/src/main/java/com/knitelius/jaog/annotations/CSVField.java
+++ jaog/src/main/java/com/knitelius/jaog/annotations/CSVField.java
@@ -6,8 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface CSVField {
-	String title();
+	String title() default "";
 	String format() default "";
+	String targetCSV() default "ALL";
 }
